76d1cf891a91003c9c80b0a1ca3d800bc58aacf6
stfg.prof <stfg.prof@gmail.com>
2023-01-10 02:38:19 -0300

    layout:div tomando el rol de body para estilos globales  navbar: iconos y links de  gits

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -4,14 +4,14 @@ import "../styles/global.css";
 import Navbar from "./navbar";
 const Layout = (props) => {
   return (
-    <>
+    <div className="bg-[#e2e2e2] w-screen h-screen">
       <Navbar />
-      <main className="container mx-auto text-justify">
+      <main className="container mx-auto text-justify ">
       <h1 className="bg-[#1da1f2] text-white"> {props.titulo}</h1>
 
       {props.children}
       </main>
-    </>
+    </div>
   );
 };
 export default Layout;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "gatsby";
 import { useState } from "react"; // import state
+import logoGitLab from "../images/gitlab-logo-500.png";
+import logoGiHub from "../images/github-mark.svg";
 
 const Navbar = () => {
   const [estadoMenuHamburguesa, setMenuHamburguesa] = useState(false);
@@ -12,79 +14,83 @@ const Navbar = () => {
   return (
     <>
       {/* justify-around  items-center justify-between flex*/}
-      <nav className=" sm:flex flex-wrap  p-4 justify-between">
-          <div className="block sm:flex ">
-            <div className="flex justify-between self-center ">
-              <Link to="/" className="p-4  font-bold ">
-                <span className=" text-2xl">ICO</span>
-              </Link>
-              <button
-                onClick={toggleHamburguesa}
-                className="sm:hidden inline-block p-4"
-              >
-                {estadoMenuHamburguesa ? "MENU" : "X"}
-              </button>
-            </div>
-
-            <ul
-              className={
-                "p-4 text-center text-2xl font-bold " +
-                (estadoMenuHamburguesa ? " hidden" : "block sm:flex")
-              }
+      <nav className=" sm:flex flex-wrap  px-4  md:px-8 lg:px-10 justify-between">
+        <div className="block sm:flex ">
+          <div className="flex justify-between  ">
+            <Link to="/" className="p-4  font-bold ">
+              <span className=" text-2xl">ICO</span>
+            </Link>
+            <button
+              onClick={toggleHamburguesa}
+              className="sm:hidden inline-block p-4"
             >
-              <li>
-                <Link
-                  to="/tags/arte/"
-                  className="block mt-4 sm:inline-block sm:mt-0 text-teal hover:text-white mr-4 "
-                >
-                  arte
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/tags/programacion"
-                  className="block mt-4 sm:inline-block sm:mt-0 text-teal hover:text-white mr-4"
-                >
-                  programacion
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/about"
-                  className="block mt-4 sm:inline-block sm:mt-0 text-teal hover:text-white mr-4"
-                >
-                  about
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/tags"
-                  className="block mt-4 sm:inline-block sm:mt-0 text-teal hover:text-white mr-4"
-                >
-                  tags
-                </Link>
-              </li>
-            </ul>
+              {estadoMenuHamburguesa ? "MENU" : "X"}
+            </button>
           </div>
+
           <ul
             className={
-              "p-4 text-center text-2xl font-bold " +
-              (estadoMenuHamburguesa ? " hidden" : "flex justify-center")
+              "p-4 text-center text-2xl font-bold sm:ml-10 " +
+              (estadoMenuHamburguesa ? " hidden" : "block sm:flex")
             }
           >
-            <Link
-              to="/tags"
-              className="block mt-4 sm:inline-block sm:mt-0 text-teal hover:text-white mr-4"
-            >
-              GIT
-            </Link>
-            <Link
-              to="/tags"
-              className="block mt-4 sm:inline-block sm:mt-0 text-teal hover:text-white mr-4"
-            >
-              GIT
-            </Link>
+            <li>
+              <Link
+                to="/tags/arte/"
+                className="block mt-4 sm:inline-block sm:mt-0 text-teal hover:text-white mr-4 "
+              >
+                arte
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/tags/programacion"
+                className="block mt-4 sm:inline-block sm:mt-0 text-teal hover:text-white mr-4"
+              >
+                programacion
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/about"
+                className="block mt-4 sm:inline-block sm:mt-0 text-teal hover:text-white mr-4"
+              >
+                about
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/tags"
+                className="block mt-4 sm:inline-block sm:mt-0 text-teal hover:text-white mr-4"
+              >
+                tags
+              </Link>
+            </li>
           </ul>
+        </div>
+        <ul
+          className={
+            "p-4 text-center text-2xl font-bold " +
+            (estadoMenuHamburguesa ? " hidden" : "flex justify-center")
+          }
+        >
+          <a
+            href="https://gitlab.com/stfg.prof"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block mt-4 sm:inline-block sm:mt-0 text-teal hover:text-white mr-4"
+          >
+            <img src={logoGitLab} alt="Gitlab" className="w-8 h-8"/>
+          </a>
+          <a
+            href="https://github.com/santitfg"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block mt-4 sm:inline-block sm:mt-0 text-teal hover:text-white mr-4"
+          >
+            <img src={logoGiHub} alt="Github"  className="w-8 h-8"/>
+          </a>
+        </ul>
       </nav>
     </>
   );
