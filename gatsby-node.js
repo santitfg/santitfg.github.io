ecebfe93c1ec4618917da0877c6a0b432c819f45
stfg.prof <stfg.prof@gmail.com>
2023-01-15 16:57:30 -0300

    sub url  para preveer errores con gh-pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,7 +9,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
   if (node.internal.type === "MarkdownRemark") {
     const slug = createFilePath({ node, getNode, basePath: "content" });
-    createNodeField({ node, value: slug, name: "slug" });
+    createNodeField({ node, value: `/posts${slug}`, name: "slug" });
   }
 };
 
