9f759383dd3196574fd86099193784b541a7830a
stfg.prof <stfg.prof@gmail.com>
2023-01-21 01:46:10 -0300

    Updates

---
+++ component---src-templates-template-tags-jsx-3c3f620b884575e1ae2d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksantitfg_github_io=self.webpackChunksantitfg_github_io||[]).push([[906],{4837:function(e,t,a){a.r(t);var l=a(7294),n=a(7302),r=a(8957);t.default=e=>{let{pageContext:t,data:a}=e;return l.createElement(r.Z,null,l.createElement("main",null,l.createElement("div",{className:"grid grid-cols-1 lg:grid-cols-2 gap-4"},a.allMarkdownRemark.edges.map((e=>{let{node:t}=e;return l.createElement(n.Z,{data:t,key:t.fields.slug})})))))}}}]);
+//# sourceMappingURL=component---src-templates-template-tags-jsx-3c3f620b884575e1ae2d.js.map
